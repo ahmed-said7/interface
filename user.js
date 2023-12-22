3a2941f66afaeeb44ac6a30d789377189c3e7925
Ahmed Said <engahmedsaid125@gmail.com>
2023-12-22 14:26:24 +0200

    1st commit

--- user.js
+++ user.js
@@ -6,14 +6,14 @@ router.get('/',async function(req,res,next){
     res.status(201).json({status: 'success',rows});
 });
 router.get('/follower',async function(req,res,next){
-    const {rows} =await pool.query(`SELECT * FROM follower JOIN users ON
+    const {rows} =await pool.query(`SELECT user_id,username AS follower FROM follower JOIN users ON
     users.id = follower.follower_id WHERE follower.user_id =${1}`);
     res.status(201).json({status: 'success',rows});
 });
 router.get('/count-follower',async function(req,res,next){
     const {rows} =await pool.query(`SELECT username,COUNT(*) 
     FROM follower JOIN users ON
-    users.id = follower.user_id GROUP BY follower.user_id HAVING COUNT(*)>=1`);
+    users.id = follower.user_id GROUP BY users.username HAVING COUNT(*)>=1`);
     res.status(201).json({status: 'success',rows});
 });
 module.exports=router;
